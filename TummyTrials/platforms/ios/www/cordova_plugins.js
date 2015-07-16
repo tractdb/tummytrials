4630bd5ca3b64a77e0026066ac2bebb962a12cd6
Jeffrey Scofield <jeffsco@psellos.com>
2015-07-16 16:43:01 -0700

    More metadata for Couchbase Lite

--- TummyTrials/platforms/ios/www/cordova_plugins.js
+++ TummyTrials/platforms/ios/www/cordova_plugins.js
@@ -34,6 +34,13 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/com.couchbase.lite.phonegap/www/cblite.js",
+        "id": "com.couchbase.lite.phonegap.CouchbaseLite",
+        "clobbers": [
+            "window.cblite"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -43,7 +50,8 @@ module.exports.metadata =
     "cordova-plugin-console": "1.0.1",
     "cordova-plugin-device": "1.0.1",
     "cordova-plugin-splashscreen": "2.1.0",
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.couchbase.lite.phonegap": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
