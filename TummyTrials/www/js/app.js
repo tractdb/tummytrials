cc9fd65f527f33fc753e47269978052a42df12c6
Ianturner1989 <Ianturner1989@gmail.com>
2015-08-17 16:02:05 -0700

    delete console log statement

--- TummyTrials/www/js/app.js
+++ TummyTrials/www/js/app.js
@@ -192,10 +192,9 @@ app.controller( "setupcontroller", function( $scope, $http, $sce) {
             numAssigned ++;
           }
         }
-        
+
         this.paramsSet = true;
         this.timesPressed++;
-        console.log(this.calander);
       }
     };
 
