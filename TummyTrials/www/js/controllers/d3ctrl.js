5eec65483147b399bb75e000c51944ae0ffad687
karkar <ravikarkar@gmail.com>
2016-03-02 16:33:50 -0800

    Vis works now
    Vis works. Can toggle between old and new vis.
    
    Need to fix toggle to work with all ng-repeats

--- TummyTrials/www/js/controllers/d3ctrl.js
+++ TummyTrials/www/js/controllers/d3ctrl.js
@@ -60,20 +60,27 @@
 
           var circleR = 7;
 
-          // Set the ranges
-          var x = d3.scale.linear()
-              .range([0, width]);
-
-          var y = d3.scale.linear()
-              .range([height, 0]);
+          var yscale = d3.scale.linear()
+                          .domain([0,8])
+                          .range([height,0]);
+          var xscale = d3.scale.ordinal()
+                          .domain(scope.data.map(function(d) { return d.condition; }))
+                          .rangeRoundBands([0,width]);
 
           // Define the axes
           var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom");
+              .scale(xscale)
+              .orient("bottom")
+              .tickFormat(function (d) {
+                var mapper = {
+                  0 : "Condition a",
+                  1 : "Condition b"
+                }
+                return mapper[d]
+              });
 
           var yAxis = d3.svg.axis()
-              .scale(y)
+              .scale(yscale)
               .orient("left")
               .ticks(9)
               .tickFormat(function (d) {
@@ -98,14 +105,6 @@
               .attr("height", height + margin.top + margin.bottom)
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-          // console.log("Data : " + typeof(scope.data));
-          // Scale the range of the data
-          x.domain(d3.extent(scope.data, function(d) { 
-            return parseInt(d.index); 
-          }));
-          y.domain([0, 8]);
 
           // remove all previous items before render
           svg.selectAll('*').remove();
@@ -134,26 +133,107 @@
               .style("text-anchor", "end")
               .text("Severity");
 
-          // Add the squares
-          svg.selectAll("circle")
-              .data(scope.data)
-            .enter().append("circle")
-                .attr("class", ".dot")
-                // .attr("d", d3.svg.symbol().type("circle").size([150]))
-                .attr("cx", function(d){return x(d.index);})
-                .attr("cy", function(d){return y(d.severity);})
-                .attr("r", circleR)
-                .style("fill", function(d){
-                    if(d.severity > 1){
-                      return color(d.condition); 
-                    } else if(d.severity == 0 || d.severity == 1){
-                      return "grey";
-                    }
-                })
-                .on("click", function(d, i){return scope.onClick({data_pt: d});});
+          var groups = {};
+          var circleR = 7;
+          var discreteTo = (circleR * 2) / (yscale.range()[0] / yscale.domain()[1]);
+          // console.log(discreteTo);
+          scope.data.forEach (function(datum) {
+              var g = Math.floor (datum.severity / discreteTo);
+              var cat = datum.condition;
+              var ref = cat+"-"+g;
+              if (!groups[ref]) { groups[ref] = 0; }
+              datum.groupIndex = groups[ref];
+              datum.discy = yscale (g * discreteTo);  // discrete.
+              groups[ref]++;
+          });
+          scope.data.forEach (function(datum) {
+              var cat = datum.condition;
+              var g = Math.floor (datum.severity / discreteTo);
+              var ref = cat+"-"+g;
+              datum.offset = datum.groupIndex - ((groups[ref] - 1) / 2);
+          });
+
+          svg.selectAll("circle").data(scope.data)
+          .enter()
+          .append("circle")
+            .attr("cx", function(d) { return (margin.left - circleR/2 + 1)  + xscale(d.condition) + (d.offset * (circleR * 3)); }) // change the "3" to vary spacing between points. 2 is 0 spacing since diameter.
+            .attr("r", circleR)
+            .attr("cy", function(d) { return d.discy - circleR; })
+            .style ("fill", function(d) { 
+                if(d.severity > 1){
+                  return color(d.condition); 
+                } else if(d.severity == 0 || d.severity == 1){
+                  return "grey";
+                }
+
+              })
+          .on("click", function(d, i){return scope.onClick({data_pt: d});});
+
+
+
         
           scope.control.updateVis = function(){
 
+            var xscale = d3.scale.linear()
+                            .range([0, width])
+                            .domain(d3.extent(scope.data, function(d) { 
+                              return parseInt(d.index); 
+                            }));
+
+            var xAxis = d3.svg.axis()
+                          .scale(xscale)
+                          .orient("bottom");                            
+
+            var yscale = d3.scale.linear()
+                            .domain([0,8])
+                            .range([height,0]);
+
+            var yAxis = d3.svg.axis()
+              .scale(yscale)
+              .orient("left")
+              .ticks(9)
+              .tickFormat(function (d) {
+                var mapper = {
+                  0 : "No report",
+                  1 : "Missing data",
+                  2 : "Not at all",
+                  3 : "Slightly",        
+                  4 : "Mildly",
+                  5 : "Moderately",
+                  6 : "Severely",
+                  7 : "Very severely",
+                  8 : "Extremely"
+                }
+                return mapper[d]
+            });
+
+            // Select the section we want to apply our changes to
+            // var svg = d3.select(element[0]).transition();
+
+            // Make the changes
+            // Change X Axis
+            svg.select(".x.axis") 
+              .transition()
+              .duration(750)
+              .call(xAxis);
+
+            //     // Change Y Axis
+            // svg.select(".y.axis")
+            //   .duration(750)
+            //   .call(yAxis);
+
+            svg.selectAll("circle")
+                    // .attr("class", ".dot")
+                    .transition()
+                    .attr("cx", function(d){
+                      return xscale(d.index); 
+                    })
+                    // .on("click", function(d, i){return scope.onClick({data_pt: d});})
+                    .duration(750);
+          //end updateVis function
+          };
+
+          scope.control.revertVis = function(){
             var xscale = d3.scale.ordinal()
                             .domain([0,1])
                             .rangeRoundBands([0,width]);
@@ -175,54 +255,29 @@
                           });
 
             // Select the section we want to apply our changes to
-            var svg = d3.select(element[0]).transition();
+            // var svg = d3.select(element[0]).transition();
 
             // Make the changes
             // Change X Axis
             svg.select(".x.axis") 
+              .transition()
               .duration(750)
               .call(xAxis);
 
-                // Change Y Axis
-            svg.select(".y.axis")
-              .duration(750)
-              .call(yAxis);
-
-
-            var groups = {};
-            var circleR = 7;
-            var discreteTo = (circleR * 2) / (yscale.range()[0] / yscale.domain()[1]);
-            // console.log(discreteTo);
-            scope.data.forEach (function(datum) {
-                var g = Math.floor (datum.severity / discreteTo);
-                var cat = datum.condition;
-                var ref = cat+"-"+g;
-                if (!groups[ref]) { groups[ref] = 0; }
-                datum.groupIndex = groups[ref];
-                datum.discy = yscale (g * discreteTo);  // discrete.
-                groups[ref]++;
-            });
-            scope.data.forEach (function(datum) {
-                var cat = datum.condition;
-                var g = Math.floor (datum.severity / discreteTo);
-                var ref = cat+"-"+g;
-                datum.offset = datum.groupIndex - ((groups[ref] - 1) / 2);
-            });
-
-            svg.selectAll(".dot")
+            svg.selectAll("circle")
+            .transition()
             // .attr("d", "circle")
-            .attr("transform", function(d){
+            .attr("cx", function(d){
                     // change the "3" to vary spacing between points. 2 is 0 spacing since diameter.
-                    x_move = (margin.left - circleR/2 - 1) + xscale(d.condition) + (d.offset * (circleR * 3)); 
-                    y_move = d.discy - circleR;
-                    var position = "translate(" + x_move + "," + y_move + ")";
-                   return position;
+                   return (margin.left - circleR/2 + 1)  + xscale(d.condition) + (d.offset * (circleR * 3)); 
+                    // var y_move = d.discy - circleR;
+                    // var position = "translate(" + x_move +   ",0)";
+                   // return x_move;
                 }) 
             .duration(750);
 
-
-          //end updateVis function
-          };
+          // end revertVis function    
+          };  
 
 
         // end d3 service
