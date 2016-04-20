7932d43bbca37647906e9297b47462daeda28d41
karkar <ravikarkar@gmail.com>
2016-04-19 23:19:53 -0700

    Results page cleaned up. Summary description and average result sentences in place. Need to work on storing results in db.

--- TummyTrials/www/js/controllers/pasttrial1ctrl.js
+++ TummyTrials/www/js/controllers/pasttrial1ctrl.js
@@ -16,6 +16,12 @@
 // }, ...
 // ]
 
+// Things needed for the results summary
+// Evidence : categorization of p-value into no evidence, possible evidence, or strong evidence
+// Symptom : name of the symptom (results are divided per symptom)
+// Change : does it improve or worsen the symptom
+// Trigger : name of trigger (common across symptoms)
+
 'use strict';
 
 (angular.module('tummytrials.pasttrial1ctrl', 
@@ -32,6 +38,7 @@
     })
     .then(function(_) {
         var study = $scope.study_previous[$stateParams.studyIndex];
+        var text = $scope.text;
         $scope.study_past1 = study;
 
         // variables for displaying test in the results
@@ -71,12 +78,22 @@
         var sym_data = {};
         var res_all = {};
 
+        //Statement summary of the result
+        var res_desc = text.results.summary;
+        var trig = cur.trigger;
+
+        // checking if trigger has Eating or Drinking in its text
+        // 'Drinking Caffeine' becomes 'Caffeine'
+        if(trig.indexOf('Eating') != -1){
+            trig = trig.replace('Eating ', '');
+        } else if(trig.indexOf('Drinking') != -1){
+            trig = trig.replace('Drinking ', '');
+        }
+        $scope.trig = trig;
+
         // iterating over all the symptoms
         for(var a = 0; a < sym_num; a++){
-
-            // iterating over all the days of the trial
-            for(i=0; i < $scope.duration_readable; i++){
-
+            sym_name = cur.symptoms[a];
                 // iterating over all the days of the trial
                 for(var i=0; i < $scope.duration_readable; i++){
                     var day = new Date((cur.start_time + (86400 * i)) * 1000);  //86400 adds 1 day
@@ -112,19 +129,22 @@
                     }
                     // Using the date as the key, store the condition and the score for each day
                     sym_data["date"] = dt;
+
+                    res_desc = res_desc.replace('{SYMPTOM}', sym_name);
+                    res_desc = res_desc.replace('{TRIGGER}', trig);
+                    sym_data["summary"] = res_desc;
                     sym_sym.push(sym_data);
                     d = [];
                     score = null;
                     sym_data = {};
+                    // need to reset the description text since {SYMPTOM} no longer exists after first pass
+                    res_desc = text.results.summary;
                 }
                 $scope.sym_syma = sym_sym;
-            }
-            res_all[cur.symptoms[a]] = sym_sym; 
-            sym_sym = [];
+                res_all[sym_name] = sym_sym; 
+                sym_sym = [];
         }
         $scope.visdata = res_all;
-
-        // $scope.view_title = Vis.view_title;
 
         $scope.$watch(Vis.view_title, function(newVal, oldVal, scope){
             if(newVal){
