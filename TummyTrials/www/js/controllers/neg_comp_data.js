d8eeb49dd389e148970deabec8576bd8fcb6df51
karkar <ravikarkar@gmail.com>
2016-07-03 00:03:15 -0700

    Textarea in note page resizes. Neg_comp page linked with result. Updated missing data text.

---
+++ TummyTrials/www/js/controllers/neg_comp_data.js
@@ -0,0 +1,13 @@
+// neg_comp_data.js     Shared data for results
+//
+// This absurdly simple service returns a single object to be shared
+// among the study result controllers.
+//
+
+// 'use strict';
+
+(angular.module('tummytrials.neg_comp_data', [])
+    .factory('Neg_Comp_Data', function() {
+        return { };
+    })
+);
